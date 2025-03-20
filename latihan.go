6ca0b1b7926bb81e8c9cb1967b3a298322903f1e
Farhan Hamzah <177802321+farhan-hamzah@users.noreply.github.com>
2025-03-20 20:46:52 +0700

    Update latihan.go

--- latihan.go
+++ latihan.go
@@ -1,23 +1,23 @@
 // Soal Kamera Dengan Procedure
-// package main
-// import "fmt"
-// func zoomIn(c int, x, y *int ){
-// 	*x = (*x)*(c)
-// 	*y = *y*c
-// 	fmt.Print(*x, " ", *y)
-// }
-// func zoomOut(c int, x, y *int ){
-// 	*x = *x/c
-// 	*y = *y/c
-// 	fmt.Print(-*x, " ", -*y)
-// }
-// func main(){
-// 	var a, b, c1 int
-// 	fmt.Scan(&a, &b)
-// 	fmt.Scan(&c1)
-// 	if c1 > 0 {
-// 		zoomIn(c1, &a, &b)
-// 	}else{
-// 		zoomOut(c1, &a, &b)
-// 	}
-// }
+package main
+import "fmt"
+func zoomIn(c int, x, y *int ){
+	*x = (*x)*(c)
+	*y = *y*c
+	fmt.Print(*x, " ", *y)
+}
+func zoomOut(c int, x, y *int ){
+	*x = *x/c
+	*y = *y/c
+	fmt.Print(-*x, " ", -*y)
+}
+func main(){
+	var a, b, c1 int
+	fmt.Scan(&a, &b)
+	fmt.Scan(&c1)
+	if c1 > 0 {
+		zoomIn(c1, &a, &b)
+	}else{
+		zoomOut(c1, &a, &b)
+	}
+}
